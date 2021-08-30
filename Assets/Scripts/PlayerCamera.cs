b6bf620c7d61f7256b0b82b7361a8b734b0c1cf4
Me NTB <truemeccs@gmail.com>
2021-08-30 15:42:10 +0200

    first

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform player;
+    public float smooth = 0.001f;
+    public float height;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 pos = new Vector3();
+        pos.x = player.position.x;
+        pos.y = player.position.y + height;
+        pos.z = player.position.z - 8.0f;
+        transform.position = pos;
+    }
+}
